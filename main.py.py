d3684cbe216a0b76f05c2dd97d12bc3370aa0727
Vaishnavi Shinde <143090492+vaishnavishinde25@users.noreply.github.com>
2025-07-30 14:15:56 +0530

    Add files via upload

---
+++ main.py.py
@@ -0,0 +1,12 @@
+
+from fastapi import FastAPI
+from routes import blog_router
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Blog API"}
+
+app.include_router(blog_router)
+
